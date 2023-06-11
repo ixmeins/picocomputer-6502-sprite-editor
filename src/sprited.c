681746ac8ae09ed975eadc4519b209d4e5d59171
ixmeins <ingmar.meins@gmail.com>
2023-06-11 14:03:37 +1000

    Added work around for upstream pixel 0 rendering bug

--- src/sprited.c
+++ src/sprited.c
@@ -1,9 +1,21 @@
+/**
+ * A simple sprite or character editor for the Picocomputer 6502 by @rumbledthumps (see youtube series).
+ * 
+ * Ingmar Meins (wtf) 2023
+ * Various borrowed bit acknowledged etc blah blah.
+ * 
+ * As of 11 June 2023 there is a video rendering bug upstream which prevents pixel 0 from being rendered in each line.
+*/
 #include <rp6502.h>
 #include <stdio.h>
 #include "font8x8.h" // covers from ASCII 32 to 95 inclusive.
 
 #define WIDTH 320
 #define HEIGHT 240 // 180 or 240
+#define LB 1 // Left border which should be zero if it were not for the rendering bug
+#define RB 319
+#define TB 0
+#define BB 239
 
 // The following assumes the original 16 colour ANSI palette. Now doubt we will
 // be able to select palettes in the future.
@@ -25,9 +37,9 @@ static void vmode(uint16_t data)
  * 
  * Clear the graphics screen memory
 */
-static void gcls(uint8_t c)
-{
+static void gcls(uint8_t c) {
     unsigned i = 0;
+
     c += c << 4; // fill the whole byte ie 2 pixels with colour.
 
     // Partially unrolled loop is FAST
@@ -79,15 +91,12 @@ static void wait()
  * Set a pixel at the specified coordinates using the colour c (0-15).
 */
 void setxyc(uint16_t x, uint8_t y, int8_t c) {
-    uint16_t vram;
-
     // c = c & 15; // Only keep colours 0-15 or assume the user has a brain and save the cpu cycles
-    vram = (y * 160) + (x >> 1); 
     
-    RIA_ADDR0 = vram; // Start address
-    // RIA_STEP0 = 0;  // We don't actually care about the vram address in RIA_ADDR0 incrementing
+    RIA_ADDR0 = (y * 160) + (x >> 1); // Start address
+    RIA_STEP0 = 0;  // We don't actually care about the vram address in RIA_ADDR0 incrementing
 
-    if (x & 1)
+    if ((x & 1) == 0)
         RIA_RW0 = (RIA_RW0 & 0xf0) | c; // right pixel in vram byte
     else
         RIA_RW0 = (RIA_RW0 & 0x0f) | (c << 4); // left pixel in vram byte
@@ -100,12 +109,10 @@ void setxyc(uint16_t x, uint8_t y, int8_t c) {
 */
 void line(uint16_t x0, uint8_t y0, uint16_t x1, uint8_t y1, uint8_t c) {
     uint8_t rightpix = 0; // 1 if right pixel else 0 for left pixel
+    uint8_t pair = 0;
 
     // Make both nibbles in the colour the same for later.
     c += c << 4;
-
-    // Determine if this is a left or right nibble pixel
-    rightpix = x0 & 1; 
 
     if (x0 == x1) { // vertical line
         // A vertical line needs a vram start address in RIA_ADDR0 and a step size of 0 in
@@ -114,11 +121,16 @@ void line(uint16_t x0, uint8_t y0, uint16_t x1, uint8_t y1, uint8_t c) {
         // to write for this pixel.
         RIA_ADDR0 = (y0 * 160) + (x0 >> 1); // address of vram pixel pair
         RIA_STEP0 = 0;
+        
+        // Determine if this is a left or right nibble pixel
+        rightpix = x0 & 1;
 
         while (y0 < y1+1) {
-            if (rightpix) {
+            if (rightpix == 0) {
+                //RIA_ADDR0 = (y0 * 160) + (x0 >> 1); // address of vram pixel pair
                 RIA_RW0 = (RIA_RW0 & 0xf0) | (c & 0x0f);
             } else {
+                //RIA_ADDR0 = (y0 * 160) + (x0 >> 1); // address of vram pixel pair
                 RIA_RW0 = (RIA_RW0 & 0x0f) | (c & 0xf0);
             }
 
@@ -131,7 +143,7 @@ void line(uint16_t x0, uint8_t y0, uint16_t x1, uint8_t y1, uint8_t c) {
         RIA_STEP0 = 0;
 
         while (x0 < (x1+1)) { // As x is changing we need to keep track of left/right nibbles each loop
-            if (x0 & 1) {
+            if ((x0 & 1) == 0) {
                 RIA_RW0 = (RIA_RW0 & 0xf0) | (c & 0x0f);
             } else {
                 RIA_RW0 = (RIA_RW0 & 0x0f) | (c & 0xf0);
@@ -154,10 +166,11 @@ void drawLayout() {
     gcls(BGCOL);
 
     // Outside border
-    line(0,0,319,0,FGCOL+1);
-    line(0,0,0,239,FGCOL);
-    line(0,239,319,239,FGCOL+1);
-    line(319,0,319,239,FGCOL);
+    line(LB,TB,LB,BB,FGCOL); // vertical line at right
+    line(RB,TB,RB,BB,FGCOL); // Vertical line at left
+
+    line(LB,TB,RB,TB,FGCOL); // horizontal line at top
+    line(LB,BB,RB,BB,FGCOL); // horizontal line at bottom
 
 }
 
@@ -173,6 +186,6 @@ void main()
 #endif
 
     drawLayout();
-    wait();
 
+    wait();
 }
